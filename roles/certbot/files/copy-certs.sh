ebe8c920494ae4378a7562420f98ee448c13a4d3
KoiFresh <kai@mayer.sh>
2024-03-19 19:01:58 +0100

    feat: remove ansible

--- roles/certbot/files/copy-certs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /etc/letsencrypt/live/server.dieklingel.com/chain.pem /etc/mosquitto/ca_certificates/chain.pem
-cp /etc/letsencrypt/live/server.dieklingel.com/cert.pem /etc/mosquitto/certs/cert.pem
-cp /etc/letsencrypt/live/server.dieklingel.com/privkey.pem /etc/mosquitto/certs/privkey.pem
-chmod 604 /etc/mosquitto/ca_certificates/chain.pem /etc/mosquitto/certs/cert.pem /etc/mosquitto/certs/privkey.pem
